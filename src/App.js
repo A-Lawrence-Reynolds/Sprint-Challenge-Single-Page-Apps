cd57898c2d59ea15ec7d473cc7c849a105bae9a4
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2019-09-20 15:09:36 -0700

    Made routs and links work.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from "react";
 import Header from "./components/Header.js";
 import CharactarList from "./components/CharacterList";
 import WelcomePage from "./components/WelcomePage";
-import SearchForm from "./components/SearchForm";
 import { Route } from "react-router-dom";
 export default function App() {
   return (
     <main>
       <Header />
       <WelcomePage />
-      <SearchForm />
       <Route
-        exact
-        path="/Character/:id"
+        path="/CharacterList"
         render={props => <CharactarList {...props} />}
       />
     </main>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="ui centered">
+      <nav>
+        <div>
+          <Link to="/"> Home</Link>
+          <Link to="/CharacterList"> Hey Over Here</Link>
+        </div>
+      </nav>
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
     </header>
   );
