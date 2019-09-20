a3e2b4bc4be21433c9ffefff24026d7fcf16254c
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2019-09-20 08:54:26 -0700

    working on getting axios call to work, 1st commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharactarList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+import SearchForm from "./components/SearchForm";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <WelcomePage />
+      <SearchForm />
+      <CharactarList />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function CharacterList() {
+const CharacterList = props => {
+  const [character, setCharacter] = useState([]);
   // TODO: Add useState to track data from useEffect
 
   useEffect(() => {
+    const getcharacters = () => {
+      axios
+        .get("https://rick-api.herokuapp.com/api/")
+        .then(response => {
+          console.log(response);
+          setCharacter(response);
+        })
+        .catch(error => {
+          console.log("houston we have a problem", error);
+        });
+    };
+    getcharacters([]);
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
-}
+  return <section className="character-list">{}</section>;
+};
+export default CharacterList;
