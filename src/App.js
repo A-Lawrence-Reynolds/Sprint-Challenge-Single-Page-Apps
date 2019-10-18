75c6b7b970ec1f9c499950eb5769cf4f6190805f
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2019-10-18 10:39:28 -0700

    Met mvp on this sprint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
-
-
+import CharactarList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+import SearchForm from "./components/SearchForm";
+import { Route } from "react-router-dom";
 export default function App() {
   return (
     <main>
       <Header />
+      <WelcomePage />
+      <Route
+        path="/CharacterList"
+        render={props => <CharactarList {...props} />}
+      />
     </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+const NavLinksDiv = styled.div`
+  text-decoration: none;
+  font-size: 20px;
+  padding: 10px;
+  border: 1px solid #c0e5c8;
+  border-radius: 5px;
+  margin: 5px;
+
+  color: #616283;
+  text-shadow: 1px 1px 1px #c0e5c8;
+`;
 
 export default function Header() {
   return (
     <header className="ui centered">
+      <nav>
+        <NavLinksDiv>
+          <Link to="/"> Home</Link>
+          <Link to="/CharacterList"> Hey Over Here</Link>
+        </NavLinksDiv>
+      </nav>
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
     </header>
   );
